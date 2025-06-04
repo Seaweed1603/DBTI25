532f472ebb415c531570b33e6c04dfb417974784
Seaweed1603 <83668029+Seaweed1603@users.noreply.github.com>
2025-06-04 11:00:51 +0000

    Driver Connection

--- JDBC/src/main/java/fhwedel/JDBC/Hello.java
+++ JDBC/src/main/java/fhwedel/JDBC/Hello.java
@@ -1,7 +1,24 @@
 package fhwedel.JDBC;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Hello {
+    private static final String CONNECTION_STRING = "jbbc:mariadb://localhost:3306/firma";
+
     public static void main(String[] args) {
-        System.out.println("Hello, Paul world!");
+        Connection connection = connectToDB(CONNECTION_STRING);
+        if (connection == null) {
+            return;
+        }
+    }
+    private static connection connectToDB(String connection_string) {
+        try {
+            Connection connection = DriverManager.getConnection(connection_string);
+        } catch (SQLException e) {
+            System.err.println("Verbindung zur Datenbank konnte nicht hergestellt werden!");
+            return null;
+        }
+        
     }
 }
